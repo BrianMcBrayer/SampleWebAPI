892e80edaa4eb3a35c9f9231dec315899b59b31f
Brian McBrayer <bmcbrayer@precisionlender.com>
2016-09-29 14:39:41 -0400

    Added Products controller and tests

---
+++ SampeWebAPITests/ProductControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using SampleWebAPI.Controllers;
+
+namespace SampeWebAPITests
+{
+    [TestFixture]
+    public class ProductControllerTests
+    {
+        [Test]
+        public void Get_NoParams_ReturnsProductList()
+        {
+            var productController = new ProductController();
+
+            var result = productController.Get();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.Not.Empty);
+        }
+
+
+        [Test]
+        public void Get_ValidParam_ReturnsProduct()
+        {
+            var productController = new ProductController();
+
+            var result = productController.Get(0);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Id, Is.EqualTo(0));
+            Assert.That(result.Name, Is.EqualTo("Red Truck"));
+            Assert.That(result.Description, Is.Not.Null);
+        }
+    }
+}
---
+++ SampleWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using SampleWebAPI.Models;
+
+namespace SampleWebAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private static readonly List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Id = 0,
+                Name = "Red Truck",
+                Description = "A red truck"
+            },
+            new Product
+            {
+                Id = 1,
+                Name = "Blue Car",
+                Description = "A blue car"
+            },
+            new Product
+            {
+                Id = 2,
+                Name = "Toaster",
+                Description = "It doesn't drive, but it toasts"
+            }
+        };
+
+        // GET: api/Product
+        public IEnumerable<Product> Get()
+        {
+            return Products;
+        }
+
+        // GET: api/Product/5
+        public Product Get(int id)
+        {
+            return Products[id];
+        }
+    }
+}
